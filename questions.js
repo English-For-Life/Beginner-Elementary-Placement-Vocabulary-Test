ecde1fcd7cc95e73006ce92b0e138c7899865316
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-06-10 10:40:27 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"Gina is married to John. He's her ____ .",
+        options:["husband", "uncle", "wife", "parent"],
+        answer: 0
+    },
+    {
+        q:"We usually ____ the shopping in a supermarket.",
+        options:["make", "do", "have", "go"],
+        answer: 1
+    },
+    {
+        q:"I love this watch! It's ____ .",
+        options:["ugly", "small", "beautiful", "cheap"],
+        answer: 2
+    },
+    {
+        q:"He doesn't have a car so he often uses public ____ .",
+        options:["car", "taxi", "bus", "transport" ],
+        answer: 3
+    },
+    {
+        q:"I don't go to ____ on Sundays.",
+        options:["work", "job", "office", "factory"],
+        answer: 0
+    },
+    {
+        q:"Do you like Chinese ____?",
+        options:["kitchen", "food", "meal", "cook"],
+        answer: 1
+    },
+    {
+        q:"They hardly ____ visit us.",
+        options:["never", "sometimes", "ever", "usually"],
+        answer: 2   
+    },
+    {
+        q:"I'm Jeff Caine. Nice to ____ you, Mr Caine.",
+        options:["speak", "talk", "watch", "meet"],
+        answer: 3
+    },
+    {
+        q:"Can I help you? Thanks, but I'm just ____ .",
+        options:["looking", "watching", "seeing", "shopping"],
+        answer: 0
+    },
+    {
+        q:"Mandy is over there. She's ____ a blue T-shirt and jeans.",
+        options:["having", "wearing", "doing", "walking"],
+        answer: 1
+    }
+
+]
